ee22e8ad61249d28a32560f06dcfe6ae4b20e4c1
Niclas Röder <nr@e-supplies.dk>
2018-06-14 21:28:32 +0200

    Tilføj og slet kunde menu færdig

--- Autovaerksted/Autovaerksted/Cars.cs
+++ Autovaerksted/Autovaerksted/Cars.cs
@@ -33,23 +33,24 @@ namespace Autovaerksted
             }
         }
 
-        public static void DeleteCars(int i)
+        public static void DeleteCars(int customerId)
         {
             var connection = new SqlConnection("Server=.\\MSSQL_SCHOOLPRAC;Database=Autovaerksted; Integrated Security = True");
-            SqlCommand cmd; connection.Close();
-            cmd = new SqlCommand("DELETE FROM Cars WHere CustomerId=@i", connection);
-            cmd.Parameters.Add("@i", System.Data.SqlDbType.Int); cmd.Parameters["@i"].Value = i;
             connection.Open();
-            int slettet = cmd.ExecuteNonQuery();
-            if (slettet > 0)
+            SqlCommand cmd = new SqlCommand($"select RegNr from Cars where CustomerId = '{customerId}'", connection);
+            using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                Console.WriteLine("Slettet - TRYK enter.");
-                Console.ReadKey();
+                while(reader.Read())
+                {
+                    //Hiv column nr 0 ud fra nuværende row
+                    Cases.DeleteCases(reader.GetValue(0).ToString());
+                }
             }
-            else
-            {
-                Console.WriteLine("Ikke fundet - TRYK enter"); Console.ReadKey();
-            }
+
+
+            cmd = new SqlCommand("DELETE FROM Cars WHere CustomerId=@i", connection);
+            cmd.Parameters.Add("@i", System.Data.SqlDbType.Int); cmd.Parameters["@i"].Value = customerId;
+            cmd.ExecuteNonQuery();
             connection.Close();
         }
     }
---
+++ Autovaerksted/Autovaerksted/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autovaerksted
+{
+    static class Menu
+    {
+        public static void AddCustomerMenu()
+        {
+            bool repeat;
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write("Fornavn: ");
+                string Firstname = Console.ReadLine();
+
+                Console.Write("Efternavn: ");
+                string Lastname = Console.ReadLine();
+
+                Console.Write("Adresse: ");
+                string CustomerAddress = Console.ReadLine();
+
+                Console.Write("Postnr: ");
+                int ZipCode = int.Parse(Console.ReadLine());
+
+                Console.Write("Email : ");
+                string Email = Console.ReadLine();
+
+                Console.Write("Mobil nummer: ");
+                string Mobile = Console.ReadLine();
+
+                Console.WriteLine("");
+                Customers.AddCustomer(Firstname, Lastname, CustomerAddress, ZipCode, Email, Mobile);
+
+
+                Console.WriteLine("Tilføj en kunde mere? Tryk 1.");
+                Console.WriteLine("Gå tilbage til main menu? - Tryk på en anden knap.");
+                ConsoleKeyInfo keyPressed = Console.ReadKey();
+                if (keyPressed.Key == ConsoleKey.D1 || keyPressed.Key == ConsoleKey.NumPad1)
+                {
+                    repeat = true;
+                }
+                else
+                {
+                    repeat = false;
+                }
+            }
+            while (repeat);
+        }
+        
+        public static void DeleteCustomerMenu()
+        {
+            Console.WriteLine("Søg efter den kunde du gerne vil slette!");
+
+            if (Customers.ShowCustomerData(Console.ReadLine()) > 0)
+            {
+                Console.WriteLine("Vælg derefter hvilken en af kunder du vil slette efter kundenummer!");
+
+                Customers.DeleteCustomer(int.Parse(Console.ReadLine()));
+            }
+            else
+            {
+                Console.WriteLine("ingen kunder");
+            }
+            Console.ReadKey();
+        }
+    }
+}
